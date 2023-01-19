fae329d98edfbdee9478560c00eae1599e277c44
Laís <88794627+daskopfkino@users.noreply.github.com>
2023-01-19 15:19:15 -0300

    Consertando código
    Percebi que tinha enviada uma versão anterior, esta é a versão finalizada.

--- day1.js
+++ day1.js
@@ -1,3 +1,5 @@
+//Dia 1 do desafio 7 days of code - Alura - 2023
+
 let lista = [1, '1', 10, '10', 30, '30']; //declara lista com elementos
 
 for (let i = 0; i < lista.length; i++) {
@@ -7,9 +9,9 @@ for (let i = 0; i < lista.length; i++) {
         if (lista[i] == lista[j]){ //ficaria melhor se fosse uma função
             console.log('Os elementos são iguais');
         } else {
-            console.log('Os elementos são diferentes);
+            console.log('Os elementos são diferentes');
         }
         //imprime os elementos e o tipo de cada um no console para que eu verifique se realmente são iguais
-        console.log(${lista[i]} é do tipo ${tipoElemento1} e ${lista[j]} é do tipo ${tipoElemento2}); 
+        console.log(`${lista[i]} é do tipo ${tipoElemento1} e ${lista[j]} é do tipo ${tipoElemento2}`); 
     }
 }
